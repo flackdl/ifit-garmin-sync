333a1f9fe73c6f792c2b4a0b7d1ac7394b6da9ee
danny <flackattack@gmail.com>
2021-01-31 15:24:50 -0600

    any host

--- src/ifit/settings.py
+++ src/ifit/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '+d@$j(3m8ryu!adlrzc0cc69ty^j(&w2zdp@^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
